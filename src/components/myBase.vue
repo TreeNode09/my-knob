a73c95dfc2a90158c2a63dc748d64f249af4c640
TreeNode09 <167090895+TreeNode09@users.noreply.github.com>
2025-08-13 11:23:09 +0800

    添加背景组件

---
+++ src/components/myBase.vue
@@ -0,0 +1,6 @@
+<template>
+<div style="display: flex; flex-direction: row; align-items: center; justify-content: center; margin: 10px;
+  padding: 5px 10px 10px 10px; background-color: var(--white); ; border-radius: 10px;
+  box-shadow: 0px 5px 10px var(--shadowDark), inset 0px -5px 2px var(--shadowMain)">
+</div>
+</template>
